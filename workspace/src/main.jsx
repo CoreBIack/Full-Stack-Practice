ffa682fb9212244cd868805091424970fb624a44
Birand Özkara <ozkarabirand@gmail.com>
2025-08-28 18:38:23 +0300

    part2 a course content done

--- workspace/src/main.jsx
+++ workspace/src/main.jsx
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+
+const notes = [
+  {id: 1, content:"HTML is easy", important: true}, 
+  {id: 2, content:"Browser can only execute Javascript", important: false},
+  {id: 3, content:"GET and POST are the most important methods of HTTP protocol", important: true}
+]
+ReactDOM.createRoot(document.getElementById("root")).render(<App notes={notes}/>)
 
 
 
