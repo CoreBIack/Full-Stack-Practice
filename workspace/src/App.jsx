20ed8cf26c294f2c69ce54f8046c44f3711dae45
Birand Özkara <ozkarabirand@gmail.com>
2025-09-02 11:09:39 +0300

    part 3 start

--- workspace/src/App.jsx
+++ workspace/src/App.jsx
@@ -1,49 +1,12 @@
-import Course from "../components/Course"
-const App =()=>{
-  const courses = [{
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4
-      }
-    ]
-    },{
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+import {useState} from "react"
+import Note from "../components/Note.jsx"
+const App =(props)=>{
+  const [notes, setNotes] = useState(props.notes)
   return <>
-    {courses.map(course => <Course key={course.id} course={course}/>)}
+    <h1>Notes</h1>
+    <ul>
+      {notes.map((note)=><Note key  ={note.id} note={note} />)}
+    </ul>
   </>
 }
 export default App
