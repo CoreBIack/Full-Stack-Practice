c736f473820312b8f27a973f7c08b7b155402f8b
Birand Özkara <ozkarabirand@gmail.com>
2025-09-04 22:36:38 +0300

    time for exercises

--- workspace/src/main.jsx
+++ workspace/src/main.jsx
@@ -1,14 +1,16 @@
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 
-const notes = [
-  {id: 1, content:"HTML is easy", important: true}, 
-  {id: 2, content:"Browser can only execute Javascript", important: false},
-  {id: 3, content:"GET and POST are the most important methods of HTTP protocol", important: true}
-]
-ReactDOM.createRoot(document.getElementById("root")).render(<App notes={notes}/>)
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
 
 
+
+
+/*axios.get("http://192.168.1.40:3001/notes").then(res=>{
+  const notes = res.data
+  console.log(notes)
+  ReactDOM.createRoot(document.getElementById("root")).render(<App notes={notes}/>)
+})*/
 
 // part1 c/practice
 /*import ReactDOM from 'react-dom/client'
