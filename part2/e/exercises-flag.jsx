99ee80a66a1fb7d8990af04486ff6bef98a2278b
Birand Özkara <ozkarabirand@gmail.com>
2025-09-20 17:37:53 +0300

    still not polished but gotta move fast

--- part2/e/exercises-flag.jsx
+++ part2/e/exercises-flag.jsx
@@ -11,7 +11,7 @@ const App=()=>{
     })
   },[])
   
-  const display = value.length > 0 ? countries.filter(country=>country.name.common.toLowerCase().includes(value.toLocaleLowerCase())) : []
+  const display = value.length > 0 ? countries.filter(country=>country.name.common.toLowerCase().includes(value.toLowerCase())) : []
 
   const handleChange=(event)=>{
     setValue(event.target.value)
@@ -23,7 +23,11 @@ const App=()=>{
       <form >
         find countries<input value={value} onChange={handleChange} type="text" />
       </form>
-      {display.length <=10 ? display.length === 1 ? display.map(country=> <Country key={country.area}name={country.name.common} capital={country.capital} area={country.area} languages={country.languages} flag={country.flags.png}/>) : display.map(country=> <li key={country.area}>{country.name.common} <button onClick={handleClick} value={country.name.common}>Show</button></li>) : "Too many matches, specify another filter"}
+      {display.length <=10 
+        ? display.length === 1 
+          ? display.map(country=> <Country key={country.area}name={country.name.common} capital={country.capital} area={country.area} languages={country.languages} flag={country.flags.png}/>) 
+          : display.map(country=> <li key={country.area}>{country.name.common} <button onClick={handleClick} value={country.name.common}>Show</button></li>) 
+        : "Too many matches, specify another filter"}
       
     </>
 }
